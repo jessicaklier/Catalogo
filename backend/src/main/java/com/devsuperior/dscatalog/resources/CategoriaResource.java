413d57c35ea4f5c9dfc56b7e3b1d9cb1988bb3eb
jessicaklier <jhessicadesouza12@hotmail.com>
2023-11-13 13:34:14 -0300

    Atualizando categoria

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoriaResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoriaResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,18 +30,23 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/{id}") // obter
 	public ResponseEntity<CategoriaDTO> findById(@PathVariable Long id){
 		CategoriaDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	@PostMapping
+	@PostMapping // inserir 
 	public ResponseEntity<CategoriaDTO> insert(@RequestBody CategoriaDTO dto){
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 		
+	}
+	@PutMapping(value = "/{id}") // atualizar
+	public ResponseEntity<CategoriaDTO> update(@PathVariable Long id, @RequestBody CategoriaDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoriaService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoriaService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoriaDTO;
 import com.devsuperior.dscatalog.entities.Categoria;
 import com.devsuperior.dscatalog.repositories.CategoriaRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
 public class CategoriaService {
@@ -29,7 +31,7 @@ public class CategoriaService {
 	@Transactional(readOnly = true)
 	public CategoriaDTO findById(Long id) {
 		Optional<Categoria> obj = repository.findById(id);
-		Categoria entity = obj.orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada"));
+		Categoria entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entidade não encontrada"));
 		return new CategoriaDTO(entity);
 	}
 
@@ -39,5 +41,18 @@ public class CategoriaService {
 		entity.setNome(dto.getNome());
 		entity =  repository.save(entity);
 		return new CategoriaDTO(entity);
+	}
+	
+	@Transactional
+	public CategoriaDTO update(Long id, CategoriaDTO dto) {
+		try {
+		Categoria entity = repository .getOne(id);
+		entity.setNome(dto.getNome());
+		entity = repository.save(entity);
+		return new CategoriaDTO(entity);
+	}
+		catch(EntityNotFoundException e) {
+		throw new ResourceNotFoundException("Identificação não encontrada" + id);
+		}
 	}
 }
